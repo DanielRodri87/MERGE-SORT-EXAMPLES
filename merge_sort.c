d5d21d20e9f422677197cc5a6aa609e4db1c9963
Daniel Rodrigues <86251321+DanielRodri87@users.noreply.github.com>
2024-07-24 17:10:39 -0300

    Update merge_sort.c

--- merge_sort.c
+++ merge_sort.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-//  O merge sort é um algoritmo de ordenação que utiliza a técnica de divisão e conquista.
+// Função principal que implementa o merge sort
 void mergesort(int *v, int n);
 
-// O sort é uma função auxiliar que ordena o vetor v[i..f] em ordem crescente.
+// Função auxiliar que realiza a recursão para dividir o vetor
 void sort(int *v, int *c, int i, int f);
 
-// O merge é uma função auxiliar que combina os vetores v[i..m] e v[m+1..f] em ordem crescente.
+// Função que mescla os sub-vetores ordenados
 void merge(int *v, int *c, int i, int m, int f);
 
 int main(void)
 {
     int i;
-    int v[4] = {38, 27, 43, 10};
+    // Vetor exemplo (substitua pelos pratos de Juninho: [6, 1, 3, 4, 2, 5])
+    int v[6] = {6, 1, 3, 4, 2, 5};
 
-    mergesort(v, 4);
+    // Chama a função mergesort para ordenar o vetor
+    mergesort(v, 6);
 
-    for (i = 0; i < 4; i++)
+    // Imprime o vetor ordenado
+    for (i = 0; i < 6; i++)
         printf("%d ", v[i]);
 
     putchar('\n');
@@ -26,65 +28,69 @@ int main(void)
     return 0;
 }
 
-/*
-  Dado um vetor de inteiros v e um inteiro n >= 0, ordena o vetor v[0..n-1] em ordem crescente.
-*/
+// Função mergesort: Inicializa um vetor auxiliar e chama a função de ordenação
 void mergesort(int *v, int n)
 {
+    // Aloca memória para o vetor auxiliar
     int *c = malloc(sizeof(int) * n);
+
+    // Chama a função de ordenação
     sort(v, c, 0, n - 1);
+
+    // Libera a memória alocada para o vetor auxiliar
     free(c);
 }
 
-/*
-  Dado um vetor de inteiros v e dois inteiros i e f, ordena o vetor v[i..f] em ordem crescente.
-  O vetor c é utilizado internamente durante a ordenação.
-*/
-
+// Função sort: Realiza a divisão recursiva do vetor
 void sort(int *v, int *c, int i, int f)
 {
+    // Caso base: Se o sub-vetor tem 1 ou 0 elementos, já está ordenado
     if (i >= f)
         return;
 
+    // Calcula o índice do meio do vetor
     int m = (i + f) / 2;
 
+    // Ordena recursivamente a primeira metade
     sort(v, c, i, m);
+
+    // Ordena recursivamente a segunda metade
     sort(v, c, m + 1, f);
 
-    /* Se v[m] <= v[m + 1], então v[i..f] já está ordenado. */
+    // Se o último elemento da primeira metade é menor ou igual ao primeiro da segunda metade, já está ordenado
     if (v[m] <= v[m + 1])
         return;
 
+    // Mescla os sub-vetores ordenados
     merge(v, c, i, m, f);
 }
 
-/*
-  Dado um vetor v e três inteiros i, m e f, sendo v[i..m] e v[m+1..f] vetores ordenados,
-  coloca os elementos destes vetores, em ordem crescente, no vetor em v[i..f].
-*/
+// Função merge: Mescla dois sub-vetores ordenados em um único vetor ordenado
 void merge(int *v, int *c, int i, int m, int f)
 {
-    int z,
-        iv = i, ic = m + 1;
+    int z, iv = i, ic = m + 1;
 
+    // Copia os elementos do vetor original para o vetor auxiliar
     for (z = i; z <= f; z++)
         c[z] = v[z];
 
     z = i;
 
+    // Mescla os dois sub-vetores ordenados
     while (iv <= m && ic <= f)
     {
-        /* Invariante: v[i..z] possui os valores de v[iv..m] e v[ic..f] em ordem crescente. */
-
+        // Compara os elementos e coloca o menor no vetor original
         if (c[iv] <= c[ic])
             v[z++] = c[iv++];
         else
             v[z++] = c[ic++];
     }
 
+    // Copia os elementos restantes da primeira metade, se houver
     while (iv <= m)
         v[z++] = c[iv++];
 
+    // Copia os elementos restantes da segunda metade, se houver
     while (ic <= f)
         v[z++] = c[ic++];
 }
